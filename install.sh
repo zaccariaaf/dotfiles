754e0c90ebf731b9bdc21097276cba4c5bcf406b
zaccariaaf <me@zaccariaaf.com>
2025-02-16 00:34:14 +0100

    minor changes

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 # PPA
 sudo add-apt-repository universe -y
-sudo add-apt-repository ppa:agornostal/ulauncher
 sudo add-apt-repository ppa:maveonair/helix-editor
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 # TOOLING
-sudo apt install git make automake cmake gcc clang clangd lldb mold curl alacritty libssl-dev perl python3 python3-pip gcc-arm-none-eabi stlink-tools helix fish
+sudo apt install git make automake cmake gcc clang clangd lldb mold curl libssl-dev perl python3 python3-pip gcc-arm-none-eabi stlink-tools fish
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install --locked zellij
 hx --grammar fetch && hx --grammar build
@@ -13,5 +12,3 @@ rustup completions fish > ~/.config/fish/completions/rustup.fish
 # LANGUAGE SERVERS
 pip install basedpyright
 cargo install taplo-cli --locked --features lsp
-# UTILS
-sudo apt install gnome-tweaks ulauncher transmission zathura zathura-pdf-poppler calibre mpv chromium syncthing fonts-firacode
